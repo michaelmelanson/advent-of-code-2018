b6d5e5741f447ed080185c6b830b1abfa987a3f8
Michael Melanson <michael@michaelmelanson.net>
2018-12-02 17:41:28 -0500

    Day 2, part 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_lib};
+use std::collections::HashMap;
 
 #[aoc(day1, part1)]
 pub fn day1_part1(input: &str) -> isize {
@@ -30,6 +31,42 @@ pub fn day1_part2(input: &str) -> isize {
       }
     }
   }
+}
+
+#[aoc(day2, part1)]
+pub fn day2_part1(input: &str) -> isize {
+  let mut twos = 0;
+  let mut threes = 0;
+
+  for line in input.lines() {
+    let mut counts: HashMap<char, isize> = HashMap::new();
+
+    for char in line.chars() {
+      *counts.entry(char).or_default() += 1;
+    }
+
+    let mut has_two = false;
+    let mut has_three = false;
+
+    for count in counts.values() {
+      if *count == 2 {
+        has_two = true;
+
+      } else if *count == 3 {
+        has_three = true;
+      }
+    }
+
+    if has_two {
+      twos += 1;
+    }
+
+    if has_three {
+      threes += 1;
+    }
+  }
+
+  twos * threes
 }
 
 aoc_lib!{ year = 2018 }
